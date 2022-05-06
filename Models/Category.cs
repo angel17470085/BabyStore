f643e9e5fa5c68a7a5ce892d633591199ebe6d31
angel17470085 <17470085@campeche.tecnm.mx>
2022-05-05 23:17:40 -0500

    Chapt:4 Finished, added product and category validation

--- Models/Category.cs
+++ Models/Category.cs
@@ -9,6 +9,9 @@ namespace BabyStore.Models
     {
         public int ID { get; set; }
 
+        [Required(ErrorMessage = "El nombre de la categoria no puede estar vacia")]
+        [StringLength(50, MinimumLength = 3,ErrorMessage ="Porfavor ingrese un nombre de 3 a 50 caracteres")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$",ErrorMessage ="Ingrese una categoria que empieze con mayusculas seguido de letras y espacios solamente")]
         [Display(Name ="Nombre Categoria")]
         public string Name { get; set; }
         public virtual ICollection<Product> Products { get; set; }
--- Models/ProductMetaData.cs
+++ Models/ProductMetaData.cs
@@ -12,12 +12,24 @@ namespace BabyStore.Models
 
     public class ProductMetaData
     {
-        [Display(Name="Producto")]
+        [Required(ErrorMessage = "El nombre del producto no puede estar en blanco")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Porfavor ingrese un nombre  de 3 a 50 caracteres")]
+        [RegularExpression(@"^[a-zA-Z0-9'-'\s]*$", ErrorMessage = "Ingrese un nombre de solo letras y numeros y espacios")]
         public string Name { get; set; }
 
-        [Display(Name="Descripcion")]
-        public string Description { get; set; }
+        [Required(ErrorMessage = "La descripcion del producto no puede estar en blanco")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Ingrese una descripcion de 10 a 200 caracteres")]
+        [RegularExpression(@"^[,;a-zA-Z0-9'-'\s]*$", ErrorMessage = "Ingrese un producto que contenga letras, numeros y espacios")]
+        [DataType(DataType.MultilineText)]
+         public string Description { get; set; }
 
+        [Required(ErrorMessage = "El precio no puede estar vacio")]
+        [Range(0.10, 10000, ErrorMessage = "Ingrese un precio entre 0.10 a 10000.00")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+
+
+        [RegularExpression("[0-9]+(\\.[0-9][0-9]?)?", ErrorMessage = "El precio debe de ser un numero con hasta dos (.) decimales")]
         [Display(Name = "Precio")]
         public decimal Price { get; set; }
     }

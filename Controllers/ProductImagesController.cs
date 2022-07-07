a3ef2ce5cc5e308e3dc2fc47034c9032aaf0dda4
angel17470085 <17470085@campeche.tecnm.mx>
2022-07-06 22:53:40 -0500

    Chap 6: Creating a partial view for creating and editing Products

--- Controllers/ProductImagesController.cs
+++ Controllers/ProductImagesController.cs
@@ -25,22 +25,8 @@ namespace BabyStore.Controllers
             return View(db.ProductImages.ToList());
         }
 
-        // GET: ProductImages/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ProductImage productImage = db.ProductImages.Find(id);
-            if (productImage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(productImage);
-        }
 
-        // GET: ProductImages/Create
+
         #endregion
 
         public ActionResult Upload()
@@ -156,36 +142,7 @@ namespace BabyStore.Controllers
 
 
          #region CRUD
-        // GET: ProductImages/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ProductImage productImage = db.ProductImages.Find(id);
-            if (productImage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(productImage);
-        }
-
-        // POST: ProductImages/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FileName")] ProductImage productImage)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(productImage).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(productImage);
-        }
+        
 
         // GET: ProductImages/Delete/5
         public ActionResult Delete(int? id)

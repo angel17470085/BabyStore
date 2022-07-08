6cc10a701b302a47149012e57b8caa5adc54cc39
angel17470085 <17470085@campeche.tecnm.mx>
2022-07-07 22:38:38 -0500

    Chap 7: Logging in as thet admin User

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -24,6 +24,13 @@ namespace BabyStore.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+        static ApplicationDbContext()
+        {
+            // Set the database initializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
 
         public static ApplicationDbContext Create()
         {

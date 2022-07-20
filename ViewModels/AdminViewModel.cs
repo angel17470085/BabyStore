5d72fc1dbe3acc8d441c2b4df74ae31b749e4137
angel17470085 <17470085@campeche.tecnm.mx>
2022-07-19 21:42:20 -0500

    Chap 7: Editing a User as Admin

--- ViewModels/AdminViewModel.cs
+++ ViewModels/AdminViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using BabyStore.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace BabyStore.ViewModels
 {
@@ -12,8 +12,37 @@ namespace BabyStore.ViewModels
         public string Id  { get; set; }
 
         [Required(AllowEmptyStrings = false)]
-        [Display(Name = "Role Name")]
+        [Display(Name = "Nombre del Rol")]
         public string Name { get; set; }
+
+    }
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre(s)")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Apellido(s)")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+
+        public Address Address { get; set; }
+        public IEnumerable<SelectListItem> RolesList { get; set; }
 
     }
 }
